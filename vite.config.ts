bbce9b0da5a55b4795669d309ee38f18ab270dc0
Hardik Makwana <hardik.makwana@o2h.com>
2026-03-13 00:05:44 +0530

    feat: Add sitemap generation using vite-plugin-sitemap and update robots.txt.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,7 +13,28 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(),
+    mode === "development" && componentTagger(),
+    Sitemap({
+      hostname: "https://pritgasupg.in",
+      dynamicRoutes: [
+        "/",
+        "/our-pgs",
+        "/gallery",
+        "/about",
+        "/contact",
+        "/pg/jodhpur-kunj-11",
+        "/pg/jodhpur-kunj-23",
+        "/pg/jodhpur-kunj-29",
+        "/pg/arun-prakash-a1",
+        "/pg/arun-prakash-b33",
+        "/pg/arun-prakash-b34",
+        "/pg/sarthi-avenue-c43",
+        "/pg/vasupujya-row-house-10"
+      ],
+    }),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
